1e3eb2a38f18b44f2736c9db3c289e22e1ce2d75
Mart7anova <mart7anova7@gmail.com>
2022-07-08 18:49:48 +0300

    add api for axios request

---
+++ src/api/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers: {
+        'API-KEY': '04167db9-f44d-4089-83a1-11d14204efcc'
+    },
+})
+
+export const userAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    },
+    follow(userId: number) {
+        return instance.post(`follow/${userId}`)
+    },
+    unfollow(userId: number) {
+        return instance.delete(`follow/${userId}`)
+    },
+}
